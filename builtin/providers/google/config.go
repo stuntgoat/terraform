0249ebaf3ca60b41bf442906d9fa2e642fd2f44a
stungtoat <isaac@overttone.com>
2014-10-18 21:06:09 -0700

    add goog files

--- builtin/providers/google/config.go
+++ builtin/providers/google/config.go
@@ -13,28 +13,25 @@ import (
 )
 
 const clientScopes string = "https://www.googleapis.com/auth/compute"
+const authURL string = "https://accounts.google.com/o/oauth2/auth"
+const tokenURL string = "https://accounts.google.com/o/oauth2/token"
 
 // Config is the configuration structure used to instantiate the Google
 // provider.
 type Config struct {
-	AccountFile       string
-	ClientSecretsFile string
-	Project           string
-	Region            string
+	AccountFile string
+	Project     string
+	Region      string
 
 	clientCompute *compute.Service
 }
 
 func (c *Config) loadAndValidate() error {
 	var account accountFile
-	var secrets clientSecretsFile
 
 	// TODO: validation that it isn't blank
 	if c.AccountFile == "" {
 		c.AccountFile = os.Getenv("GOOGLE_ACCOUNT_FILE")
-	}
-	if c.ClientSecretsFile == "" {
-		c.ClientSecretsFile = os.Getenv("GOOGLE_CLIENT_FILE")
 	}
 	if c.Project == "" {
 		c.Project = os.Getenv("GOOGLE_PROJECT")
@@ -50,24 +47,15 @@ func (c *Config) loadAndValidate() error {
 			err)
 	}
 
-	if err := loadJSON(&secrets, c.ClientSecretsFile); err != nil {
-		return fmt.Errorf(
-			"Error loading client secrets file '%s': %s",
-			c.ClientSecretsFile,
-			err)
-	}
-
 	// Get the token for use in our requests
 	log.Printf("[INFO] Requesting Google token...")
 	log.Printf("[INFO]   -- Email: %s", account.ClientEmail)
 	log.Printf("[INFO]   -- Scopes: %s", clientScopes)
 	log.Printf("[INFO]   -- Private Key Length: %d", len(account.PrivateKey))
-	log.Printf("[INFO]   -- Token URL: %s", secrets.Web.TokenURI)
 	jwtTok := jwt.NewToken(
 		account.ClientEmail,
 		clientScopes,
 		[]byte(account.PrivateKey))
-	jwtTok.ClaimSet.Aud = secrets.Web.TokenURI
 	token, err := jwtTok.Assert(new(http.Client))
 	if err != nil {
 		return fmt.Errorf("Error retrieving auth token: %s", err)
@@ -78,8 +66,8 @@ func (c *Config) loadAndValidate() error {
 		Config: &oauth.Config{
 			ClientId: account.ClientId,
 			Scope:    clientScopes,
-			TokenURL: secrets.Web.TokenURI,
-			AuthURL:  secrets.Web.AuthURI,
+			AuthURL:  authURL,
+			TokenURL: tokenURL,
 		},
 		Token: token,
 	}
@@ -99,16 +87,6 @@ type accountFile struct {
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientId     string `json:"client_id"`
-}
-
-// clientSecretsFile represents the structure of the client secrets JSON file.
-type clientSecretsFile struct {
-	Web struct {
-		AuthURI     string `json:"auth_uri"`
-		ClientEmail string `json:"client_email"`
-		ClientId    string `json:"client_id"`
-		TokenURI    string `json:"token_uri"`
-	}
 }
 
 func loadJSON(result interface{}, path string) error {
